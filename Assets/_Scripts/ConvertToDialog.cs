8def7df248b6bc055f21a7def9d31f0344203181
tommtom9 <tommtom9@live.nl>
2016-10-14 21:41:25 +0200

    Added files

---
+++ Assets/_Scripts/ConvertToDialog.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ConvertToDialog : MonoBehaviour {
+
+    [SerializeField]
+    GameObject textObject;
+    Text dialogText;
+
+    [SerializeField]
+    GameObject nextbutton;
+
+    [SerializeField]
+    GameObject responseholderGameobject;
+    Transform responseholder;
+
+    [SerializeField]
+    GameObject buttonPrefab;
+
+    List<string> conversationText = new List<string>();
+    List<string> responsesText = new List<string>();
+
+    int currentTextRange = 0;
+
+    void Start() {
+        dialogText = textObject.GetComponent<Text>();
+        responseholder = responseholderGameobject.GetComponent<Transform>();
+        nextText();
+    }
+    
+    public void recieve(List<string> ConversationText, List<string> ResponsesText) {
+        conversationText = ConversationText;
+        responsesText = ResponsesText;
+    }
+
+    public void nextText() {
+        if (currentTextRange < conversationText.Count)
+        {
+            dialogText.text = conversationText[currentTextRange];
+            currentTextRange++;
+        }
+        else {
+            ToResponses();
+        }
+    }
+
+    void ToResponses() {
+
+        nextbutton.SetActive(false);
+        for (int i = 0; i < responsesText.Count; i++) {
+            GameObject button = Instantiate(buttonPrefab, new Vector3(responseholder.position.x / responsesText.Count * (i + responsesText.Count ) , responseholder.position.y, 0), Quaternion.Euler(0, 0, 0)) as GameObject;
+            button.transform.parent = responseholder;
+
+            button.GetComponentInChildren<Text>().text = responsesText[i];
+        }
+    }
+}
---
+++ Assets/_Scripts/ImportDialogTree.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class ImportDialogTree : MonoBehaviour {
+
+    [SerializeField]
+    TextAsset xmldocument;
+
+    [SerializeField]
+    ConvertToDialog reciever;
+
+    XmlDocument xml = new XmlDocument();
+
+    List<string> conversationText = new List<string>();
+    List<string> responsesText = new List<string>();
+
+    void Awake()
+    {
+        GetTree();
+    }
+
+    void GetTree(){
+        xml.LoadXml(xmldocument.text);
+        XmlNodeList textList = xml.GetElementsByTagName("conversation");
+
+        foreach ( XmlNode conversation in textList) {
+
+            XmlNodeList content = conversation.ChildNodes;
+
+            foreach (XmlNode node in content) {
+
+                switch (node.Name)
+                {
+                    case "text":
+                        conversationText.Add(node.InnerText);
+                        break;
+
+                    case "response":
+                        responsesText.Add(node.InnerText);
+                        break;
+
+                    default:
+                        Debug.Log("Name not found");
+                        break;
+                }
+            }
+        }
+        SendToReciever();
+    }
+
+    void SendToReciever() {
+        reciever.recieve(conversationText, responsesText);
+    }
+}
